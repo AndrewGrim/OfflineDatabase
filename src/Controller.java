9bc8100e1ba7ad7b69fc8f7c7b81c897ca0c5ead
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-04-09 19:32:53 +0100

    better kelbi

--- src/Controller.java
+++ src/Controller.java
@@ -100,9 +100,6 @@ public class Controller implements Initializable {
         Monster m = databaseTable.getItems().get(index);
         int id = m.getId();
         String iconPath = "images/" + id + ".png";
-        System.out.println(iconPath);
-        System.out.println(m);
-        System.out.println(id);
 
         try {
             Image image = new Image(iconPath);
