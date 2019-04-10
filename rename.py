8d31ff6913e0da4add2cec97c43d2315febb5205
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-04-10 17:18:40 +0100

    changed png names to monster names since if you deleted a row then all the pngs would be off by one

--- rename.py
+++ rename.py
@@ -2,16 +2,73 @@ import os.path
 import os
 import shutil
 
-localDir = os.path.abspath(os.path.dirname(__file__)) + "\src\images"
+monsterNameList = ["Apceros",	
+                    "Aptonoth",
+                    "Barnos",
+                    "Gajalaka",	
+                    "Gajau",
+                    "Gastodon",
+                    "Girros",
+                    "Grimalkyne",
+                    "Hornetaur",
+                    "Jagras",	
+                    "Kelbi",	
+                    "Kestodon Male",
+                    "Kestodon Female",		
+                    "Mernos",	
+                    "Mosswine",	
+                    "Noios",	
+                    "Raphinos",	
+                    "Shamos",		
+                    "Vespoid",	
+                    "Great Jagras",		
+                    "Kulu-Ya-Ku",		
+                    "Pukei-Pukei",		
+                    "Barroth",		
+                    "Jyuratodus",		
+                    "Tobi-Kadachi",		
+                    "Anjanath",		
+                    "Rathian",	
+                    "Tzitzi-Ya-Ku",		
+                    "Paolumu",		
+                    "Great Girros",		
+                    "Radobaan",		
+                    "Legiana",		
+                    "Odogaron",		
+                    "Rathalos",		
+                    "Diablos",		
+                    "Kirin",		
+                    "Zorah Magdaros",	
+                    "Dodogama",		
+                    "Pink Rathian",		
+                    "Bazelgeuse",		
+                    "Deviljho",		
+                    "Lavasioth",		
+                    "Uragaan",		
+                    "Azure Rathalos",	
+                    "Black Diablos",		
+                    "Nergigante",		
+                    "Teostra",		
+                    "Kushala Daora",		
+                    "Vaal Hazak",		
+                    "Xeno'jiva",		
+                    "Kulve Taroth",		
+                    "Lunastra",		
+                    "Behemoth",		
+                    "Leshen",	
+                    "Ancient Leshen"] 	
+
+localDir = os.path.abspath(os.path.dirname(__file__)) + "\src\images\\50-55"
 
 dirList = os.listdir(localDir)
 
-i = 150
+i = 49 # 0, 9, 19, 29, 39, 49
 for item in dirList:
-	shutil.move(localDir + "\\" + item, localDir + "\\" + str(i) + ".png")
+	#print(item)
+	#print(monsterNameList[i])
+	print(localDir + "\\" + item)
+	print(localDir + "\\" + monsterNameList[i] + ".png")
+	shutil.move(localDir + "\\" + item, localDir + "\\" + monsterNameList[i] + ".png")
 	i += 1
-	
-i = 20
-for item in dirList:
-	shutil.move(localDir + "\\" + item, localDir + "\\" + str(i) + ".png")
-	i += 1
+	if i >= 56: #10, 20, 30, 40, 50, 56
+		break
